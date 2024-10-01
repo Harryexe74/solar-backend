41f4f6bef0ea71ca2a707bc6798f63b329774783
Harryexe74 <junaidmahmood141@gmail.com>
2024-10-01 11:16:29 +0500

    Test product

--- index.js
+++ index.js
@@ -1,11 +1,18 @@
 import config from "./config/index.js";
 import connectDB from "./config/db.js";
+import express from "express"
 
-import app from "./app.js";
+const app =express()
+// import app from "./app.js";
 
+
+app.get("/",(req,res)=>{
+	res.send("Server Live");
+})
 connectDB();
 
 const port = config.port || 4000;
+
 
 app.listen(port, () => {
 	console.log(`Server is listening on port ${port}`);

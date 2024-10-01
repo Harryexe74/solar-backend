1856cf4864211b709ddac2733132f87ae6cfc5d7
Harryexe74 <junaidmahmood141@gmail.com>
2024-10-01 11:49:42 +0500

    update 5

--- index.js
+++ index.js
@@ -1,3 +1,13 @@
-export default (req, res) => {
-	res.status(200).json({ message: "Hello from the serverless function!" });
-  };
+import config from "./config/index.js";
+import connectDB from "./config/db.js";
+
+import app from "./app.js";
+
+connectDB();
+
+const port = config.port || 4000;
+
+app.listen(port, () => {
+	console.log(`Server is listening on port ${port}`);
+});
+
